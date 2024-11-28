8ea2455e7646692c05827fea68b94b7470bc1ff9
G-h0st1n <a01645013@tec.mx>
2024-11-28 10:59:18 -0600

    added recommendation system manager

---
+++ RecommendationSystem.cpp
@@ -0,0 +1,51 @@
+//
+// Created by frpat on 11/27/2024.
+//
+
+#include "RecommendationSystem.h"
+#include <unordered_set>
+#include <iostream>
+
+void RecommendationSystem::addUser(const std::string& user) {
+    userManager.addUser(user);
+    graph.addUser(user);
+}
+
+void RecommendationSystem::addInterest(const std::string& user, const std::string& interest) {
+    userManager.addInterest(user, interest);
+}
+
+void RecommendationSystem::addFriend(const std::string& user1, const std::string& user2) {
+    graph.addFriend(user1, user2);
+}
+
+void RecommendationSystem::addContent(const std::string& category, const std::string& content) {
+    contentManager.addContent(category, content);
+}
+
+std::vector<std::string> RecommendationSystem::recommendContent(const std::string& user) {
+    std::unordered_set<std::string> allInterests;
+
+    // Obtener los amigos del usuario
+    auto friends = graph.getFriends(user);
+
+    // Obtener intereses de los amigos y agregarlos al conjunto de intereses
+    for (const auto& friendUser : friends) {
+        auto friendInterests = userManager.getInterests(friendUser);
+        for (const auto& friendInterest : friendInterests) {
+            allInterests.insert(friendInterest);  // Solo agregamos los intereses de los amigos
+        }
+    }
+
+    // Generar recomendaciones basadas en los intereses combinados
+    return contentManager.getRecommendations(allInterests);
+}
+
+// Métodos para obtener acceso a UserManager y Graph
+UserManager& RecommendationSystem::getUserManager() {
+    return userManager;
+}
+
+Graph& RecommendationSystem::getGraph() {
+    return graph;
+}
---
+++ RecommendationSystem.h
@@ -0,0 +1,34 @@
+//
+// Created by frpat on 11/27/2024.
+//
+
+#ifndef RECOMMENDATIONSYSTEM_H
+#define RECOMMENDATIONSYSTEM_H
+
+#include <string>
+#include <vector>
+#include "UserManager.h"
+#include "Graph.h"
+#include "ContentManager.h"
+
+class RecommendationSystem {
+public:
+    // Métodos principales
+    void addUser(const std::string& user);
+    void addInterest(const std::string& user, const std::string& interest);
+    void addFriend(const std::string& user1, const std::string& user2);
+    void addContent(const std::string& category, const std::string& content);
+
+    std::vector<std::string> recommendContent(const std::string& user);
+
+    // Métodos para obtener acceso a UserManager y Graph
+    UserManager& getUserManager();
+    Graph& getGraph();
+
+private:
+    UserManager userManager;
+    Graph graph;
+    ContentManager contentManager;
+};
+
+#endif // RECOMMENDATIONSYSTEM_H
