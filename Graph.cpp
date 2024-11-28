495b6ae12ad264e6e7a0637bef7600a49cf616f7
ivanabanderas <A01638966@tec.mx>
2024-11-28 11:26:32 -0600

    Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-#include "Graph.h"
-
-void Graph::addUser(const std::string& user) {
-    if (adjList.find(user) == adjList.end()) {
-        adjList[user] = {};
-    }
-}
-
-void Graph::addFriend(const std::string& user1, const std::string& user2) {
-    adjList[user1].insert(user2);
-    adjList[user2].insert(user1);
-}
-
-std::vector<std::string> Graph::getFriends(const std::string& user) {
-    std::vector<std::string> friends;
-    if (adjList.find(user) != adjList.end()) {
-        for (const auto& friendUser : adjList[user]) {
-            friends.push_back(friendUser);
-        }
-    }
-    return friends;
-}
-=======
 #include "Graph.h"
 
 void Graph::addUser(const std::string& user) {
